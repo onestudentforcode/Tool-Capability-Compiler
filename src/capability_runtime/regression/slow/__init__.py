0cf0379cca8f4eedd98a45e63f3a44e06dff0692
yushijun <884456850@qq.com>
2026-09-05 09:29:21 +0800

    feat: add phase 3 slow regression observation stats

--- src/capability_runtime/regression/slow/__init__.py
+++ src/capability_runtime/regression/slow/__init__.py
@@ -1,17 +1,37 @@
 from .route import ObservedRoute, RouteSegment, extract_observed_route
 from .runner import SlowRegressionRunner, SlowRunOutcome
+from .stats import (
+    EdgeObservationStats,
+    ExpansionDelta,
+    NodeObservationStats,
+    ObservationReport,
+    RouteObservationStats,
+    SelectionEvent,
+    build_observation_stats,
+    compute_expansion_deltas,
+    summarize,
+)
 from .trace import ExecutionTrace, LayerExecution
 from .trial import Trial, TrialExecutionStatus, TrialResult
 
 __all__ = [
+    "EdgeObservationStats",
     "ExecutionTrace",
+    "ExpansionDelta",
     "LayerExecution",
+    "NodeObservationStats",
+    "ObservationReport",
     "ObservedRoute",
+    "RouteObservationStats",
     "RouteSegment",
+    "SelectionEvent",
     "SlowRegressionRunner",
     "SlowRunOutcome",
     "Trial",
     "TrialExecutionStatus",
     "TrialResult",
+    "build_observation_stats",
+    "compute_expansion_deltas",
     "extract_observed_route",
+    "summarize",
 ]
